2f36fe8968003aed6bfcfef4187e4f1e91d72c27
Saloni Kashyap <124189127+salonikashyap7899@users.noreply.github.com>
2025-12-20 11:19:04 +0530

    new add

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 # config.py
-# Centralized constants for the application
+# 1. Binance Credentials (Never share these!)
+BINANCE_KEY = 'iyK0QCtq44CZb7K5BlRcZPCrjn2i7zeL52KQXxs9654NWkQnfQIvm1rKBaNhbXob'
+BINANCE_SECRET = 'EowxqqSJr8vD15Bk8oUGArIn9TrYaXlmPjoccV7TVLqLFZ7aqId3KzJY9l5iurOp'
 
+# 2. Risk Management Rules
 DAILY_MAX_TRADES = 4
 DAILY_MAX_PER_SYMBOL = 2
-RISK_PERCENT = 1.0          # 1%
+RISK_PERCENT = 1.0          # 1% risk per trade
 TOTAL_CAPITAL_DEFAULT = 10000.0
