cbb7e66b24075a11ef5f3de2d7e95f99f0bc549f
Saloni Kashyap <124189127+salonikashyap7899@users.noreply.github.com>
2026-06-30 18:27:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1218,4 +1218,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         ensure_sqlite_trade_positions_columns()
-    app.run(debug=True, port=5000)
+   app.run(host="0.0.0.0", port=5000, debug=True)
